7be22acf447902c9c822d4329fb3cd26c93cc011
rbhagat32 <raghavbhagat32@gmail.com>
2024-05-21 02:10:32 +0530

    added lenis

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropDrilling from "./components/PropDrilling/PropDrilling";
+import Lenis from "lenis";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <div className="w-full h-full">
       <PropDrilling />
